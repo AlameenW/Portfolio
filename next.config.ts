0a7ac424144cc4022d3aca4d7f66837e597d2862
Alameen <Adekualameem@gmail.com>
2025-12-13 21:30:15 -0600

    Turbopack issue fixed

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]",
-        },
-      },
-    });
-    return config;
-  },
+  // Empty turbopack config to silence the warning
+  turbopack: {},
 };
 
 export default nextConfig;
